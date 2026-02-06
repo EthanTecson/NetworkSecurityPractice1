5772b0e0dc1a79315747e83b7f842827b994b9ca
ethan <ethantecson@gmail.com>
2026-02-05 20:41:10 -0500

    setup done

--- Project1/project1_part2.py
+++ Project1/project1_part2.py
@@ -3,13 +3,21 @@ Project 1 Secret Key
 AES encryption algorithm
 
 Oghap, Ethan, Bri
+
+D-#s
+Ethan - D01959487
+Oghap - D01960675
+Bri - D01960691 
 """
 
 # Set random seed for reproducibility
-seed = 1960675
+seed = 1959487 
 import random
-random.seed(seed)
+random_seed = random.seed(seed)
 
+
+# generate a 128 bit key using getrandbits
+key = random.getrandbits(128)
 
 # Generate a 128 bit plaintext block
 def text_to_bits(text: str, encoding="utf-8") -> str:
@@ -17,5 +25,8 @@ def text_to_bits(text: str, encoding="utf-8") -> str:
          return ''.join(f'{byte:08b}' for byte in data)
 
 def main():
-    sorted_d_number = "D01960675"
+    sorted_d_number = "D01959487D01960675D01960691"
     text_to_bits(sorted_d_number)[:128]
+    print(text_to_bits(sorted_d_number)[:128])
+
+main()
